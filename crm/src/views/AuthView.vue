a5fe7640a5a6e5fd83d4fc425134ea7a04c72e36
hitesh sunkara <hiteshsunkara@eworldes.com>
2023-02-11 14:38:33 +0530

    auth view fix

--- crm/src/views/AuthView.vue
+++ crm/src/views/AuthView.vue
@@ -1,5 +1,56 @@
 <template>
-<h1>Signin</h1>
+  <div class="window-height window-width row justify-center items-center">
+    <q-card square class="shadow-24" style="width: 400px; height: 340px">
+      <q-card-section>
+        <div class="text-h5">Singin</div>
+        <hr />
+      </q-card-section>
+      <q-card-section>
+        <form class="q-px-sm q-pt-xl">
+          <q-input
+            ref="email"
+            outlined
+            dense
+            clearable
+            v-model="email"
+            type="email"
+            lazy-rules
+            :rules="[this.required, this.isEmail, this.short]"
+            label="Email"
+          >
+            <template v-slot:prepend>
+              <q-icon name="eva-email-outline" />
+            </template>
+          </q-input>
+          <q-input
+            ref="email"
+            outlined
+            dense
+            clearable
+            v-model="email"
+            type="password"
+            lazy-rules
+            :rules="[this.required, this.isEmail, this.short]"
+            label="password"
+          >
+            <template v-slot:prepend>
+              <q-icon name="eva-unlock-outline" />
+            </template>
+          </q-input>
+        </form>
+      </q-card-section>
+
+      <q-card-actions class="q-px-lg">
+        <q-btn
+          unelevated
+          color="secondary"
+          @click="submit"
+          class="full-width text-white"
+          label="signin"
+        />
+      </q-card-actions>
+    </q-card>
+  </div>
 </template>
 
 <script></script>
